92de698d5ada1167f4c41fa84eb30081a3288ceb
ValeryBuko89 <valerybuko@coherentsolutions.com>
2019-11-02 17:28:17 +0300

    create mockdata for List

---
+++ mock/data/data.jsx
@@ -0,0 +1,22 @@
+export const listOptions = [
+  {
+    id: 1,
+    content: 'Minsk +27'
+  },
+  {
+    id: 2,
+    content: 'Moskow +23'
+  },
+  {
+    id: 3,
+    content: 'Kiev +32'
+  },
+  {
+    id: 4,
+    content: 'Prague +30'
+  },
+  {
+    id: 5,
+    content: 'Milan +35'
+  }
+];
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button from './Components/Button/Button';
+import List from './Components/List/List';
+import { listOptions } from '../mock/data/data';
 
 class App extends React.PureComponent {
   render () {
@@ -10,6 +12,7 @@ class App extends React.PureComponent {
         <Button onClick={buttonHandleClick}>
           {'go to the page'}
         </Button>
+        <List options={listOptions}/>
       </div>
     );
   }
