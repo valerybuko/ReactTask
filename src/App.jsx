fe3b6fa3404ac27f07c990aff87101b490f44e81
ValeryBuko89 <valerybuko@coherentsolutions.com>
2019-11-03 16:00:00 +0300

    add styles for logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Button from './Components/Button/Button';
 import List from './Components/List/List';
 import { listCitiesWeatherOptions, listCitiesOptions, logoUrl } from '../mock/data/data';
+import styles from './App.scss';
 
 class App extends React.PureComponent {
   render () {
     const buttonHandleClick = () => { alert('button has been clicked'); };
     return (
       <div>
-        <img src={logoUrl} alt='logo'/>
+        <img src={logoUrl} alt='logo' className={styles.logo}/>
         <Button onClick={buttonHandleClick}>
           {'show cities'}
         </Button>
